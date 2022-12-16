6da0de62433628afa681f9f8958db0a9b4daffa3
Michael Weinrich <michael.weinrich@allianz.com>
2022-12-16 10:19:02 +0100

    Reorder folder structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # apply boot config
-sudo cp --backup=numbered boot/config.txt /boot/config.txt
+sudo cp --backup=numbered config/boot/config.txt /boot/config.txt
 
 # install necessary software
 sudo apt install -y mpd mpc alsa-utils
 
 # copy config files
-sudo cp --backup=numbered etc/asound.conf /etc/asound.conf
-sudo cp --backup=numbered etc/mpd.conf /etc/mpd.conf
+sudo cp --backup=numbered config/etc/asound.conf /etc/asound.conf
+sudo cp --backup=numbered config/etc/mpd.conf /etc/mpd.conf
