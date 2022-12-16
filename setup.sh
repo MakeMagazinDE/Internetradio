8fcd392b6a7e8e456bb26438a48d76d8144ceefd
Michael Weinrich <michael.weinrich@allianz.com>
2022-12-16 16:02:00 +0100

    Create music dir in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ fi
 # install necessary software
 sudo apt install -y mpd mpc mpg321 alsa-utils python3-pip i2c-tools
 
+# create necessary firs
+mkdir -p /home/pi/raspiradio/music
+
 # copy config files
 if ! diff -w sys_config/etc/asound.conf /etc/asound.conf > /dev/null
 then
