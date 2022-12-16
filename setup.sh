3818577bb61139afd22e03fbec8b18073f5b9d78
Michael Weinrich <michael.weinrich@allianz.com>
2022-12-16 11:29:29 +0100

    Make setup.sh a little more clever

--- setup.sh
+++ setup.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -x
 
 # apply boot config
-sudo cp --backup=numbered sys_config/boot/config.txt /boot/config.txt
+if ! diff -w sys_config/boot/config.txt /boot/config.txt > /dev/null
+then
+    sudo cp --backup=numbered sys_config/boot/config.txt /boot/config.txt
+fi
 
 # install necessary software
-sudo apt install -y mpd mpc alsa-utils python3-pip
+sudo apt install -y mpd mpc mpg321 alsa-utils python3-pip
 
 # copy config files
-sudo cp --backup=numbered sys_config/etc/asound.conf /etc/asound.conf
-sudo cp --backup=numbered sys_config/etc/mpd.conf /etc/mpd.conf
+if ! diff -w sys_config/etc/asound.conf /etc/asound.conf > /dev/null
+then
+    sudo cp --backup=numbered sys_config/etc/asound.conf /etc/asound.conf
+fi
+if ! diff -w sys_config/etc/mpd.conf /etc/mpd.conf > /dev/null
+then
+    sudo cp --backup=numbered sys_config/etc/mpd.conf /etc/mpd.conf
+fi
 
 # copy init script
-sudo cp --backup=numbered sys_config/etc/init.d/radio /etc/init.d/radio
-sudo chmod 755 /etc/init.d/radio
-sudo update-rc.d radio defaults
+if ! diff -w sys_config/etc/init.d/radio /etc/init.d/radio > /dev/null
+then
+    sudo cp --backup=numbered sys_config/etc/init.d/radio /etc/init.d/radio
+    sudo chmod 755 /etc/init.d/radio
+    sudo update-rc.d radio defaults
+fi
 
 # Python dependencies
 #pip install -r requirements.txt
