d5272a42d4a13c096545e880a1940f3e4be59e1f
Michael Weinrich <michael.weinrich@allianz.com>
2022-12-16 10:14:00 +0100

    Copy config files

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ sudo cp --backup=numbered boot/config.txt /boot/config.txt
 
 # install necessary software
 sudo apt install -y mpd mpc alsa-utils
+
+# copy config files
+sudo cp --backup=numbered etc/asound.conf /etc/asound.conf
+sudo cp --backup=numbered etc/mpd.conf /etc/mpd.conf
