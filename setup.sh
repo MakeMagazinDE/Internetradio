d050ec911ea046abf17dac36268e100a10186407
Michael Weinrich <michael.weinrich@allianz.com>
2022-12-16 10:23:56 +0100

    Setup init script

--- setup.sh
+++ setup.sh
@@ -9,3 +9,8 @@ sudo apt install -y mpd mpc alsa-utils
 # copy config files
 sudo cp --backup=numbered sys_config/etc/asound.conf /etc/asound.conf
 sudo cp --backup=numbered sys_config/etc/mpd.conf /etc/mpd.conf
+
+# copy init script
+sudo cp --backup=numbered sys_config/etc/init.d/radio /etc/init.d/radio
+sudo chmod 755 /etc/init.d/radio
+sudo update-rc.d radio defaults
